c7a273f796e4105641b2eea04e98254267d8d53b
Omolara1950 <fegomson@gmail.com>
2023-12-27 16:36:51 -0600

    Creates blog editor navbar

--- client/src/components/BlogEditor.jsx
+++ client/src/components/BlogEditor.jsx
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
+
 const BlogEditor = () => {
-  return <div>BlogEditor</div>;
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/">
+          <img src={logo} className="flex-none w-10" />
+        </Link>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2">Publish</button>
+          <button className="btn-light py-2">Save Draft</button>
+        </div>
+      </nav>
+    </>
+  );
 };
 export default BlogEditor;
